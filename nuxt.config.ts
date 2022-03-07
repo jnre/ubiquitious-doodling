d3ebb53b2ecafdd901a6b45c4ef850033027f344
jnre <josephng1993@gmail.com>
2022-03-07 20:18:55 +0800

    update halfway for navbar from awesomestarter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 import { defineNuxtConfig } from "nuxt3";
-
+import UnpluginComponentsVite from "unplugin-vue-components/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import { IntlifyModuleOptions } from '@intlify/nuxt3'
+declare module '@nuxt/schema' {
+  interface NuxtConfig {
+    intlify?: IntlifyModuleOptions
+  }
+}
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   css: ["@/assets/css/styles.css"],
@@ -8,5 +15,29 @@ export default defineNuxtConfig({
       postcssOptions: require("./postcss.config.js"),
     },
   },
-  buildModules: ['@nuxtjs/mdx', "@nuxtjs/svg"],
+  buildModules: ["@nuxtjs/mdx", "@nuxtjs/svg", "@intlify/nuxt3"],
+
+  //vite plugins
+  vite: {
+    plugins: [
+      UnpluginComponentsVite({
+        dts: true,
+        resolvers: [
+          IconsResolver({
+            prefix: "Icon",
+          }),
+        ],
+      }),
+    ],
+  },
+
+  // localization - i18n config
+  intlify: {
+    localeDir: "locales",
+    vueI18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      availableLocales: ["en", "id", "ja"],
+    },
+  },
 });
